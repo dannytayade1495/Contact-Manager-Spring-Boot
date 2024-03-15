bde0398f06d649a28e4cc93ce142f2a2fec2ac57
Dhananjay Tayade <69244801+dannytayade1495@users.noreply.github.com>
2024-03-15 11:02:40 +0530

    Update ContactmanagerApplication.java

--- contactmanager/src/main/java/com/jspiders/contactmanager/ContactmanagerApplication.java
+++ contactmanager/src/main/java/com/jspiders/contactmanager/ContactmanagerApplication.java
@@ -4,15 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableSwagger2
-//@Configuration
-//@SwaggerDefinition
 public class ContactmanagerApplication {
-
-	/*
-	 * @Bean public Docket apiDocket() { return new
-	 * Docket(DocumentationType.SWAGGER_2); }
-	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactmanagerApplication.class, args);
